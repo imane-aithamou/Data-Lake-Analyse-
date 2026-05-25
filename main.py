c50169ea62f0cb9374eb86e3bb5600cacd1e059a
imane-aithamou <aithamouimane66@gmail.com>
2026-05-25 22:06:26 +0100

    Add silver data cleaning transformations

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from pipeline.bronze_ingestion import ingerer_bronze
-from pipeline.silver_transform import charger_depuis_bronze
+from pipeline.silver_transform import (
+    charger_depuis_bronze,
+    normaliser_villes,
+    normaliser_types_contrat,
+    nettoyer_titres_postes,
+    normaliser_salaires,
+    normaliser_experience,
+    normaliser_dates,
+)
 
 
 DATA_LAKE_ROOT = "data_lake"
@@ -11,8 +19,29 @@ def main():
 
     df = charger_depuis_bronze(DATA_LAKE_ROOT)
 
-    print(df.head())
-    print(df.columns.tolist())
+    df = normaliser_villes(df)
+    df = normaliser_types_contrat(df)
+    df = nettoyer_titres_postes(df)
+    df = normaliser_salaires(df)
+    df = normaliser_experience(df)
+    df = normaliser_dates(df)
+
+    print(df[[
+        "id_offre",
+        "ville",
+        "ville_std",
+        "type_contrat",
+        "type_contrat_std",
+        "titre_poste",
+        "profil_normalise",
+        "salaire_brut",
+        "salaire_median_mad",
+        "experience_requise",
+        "experience_min_ans",
+        "annee",
+        "mois",
+        "date_valide"
+    ]])
 
 
 if __name__ == "__main__":
